e8b9d9e9956338f8f48675a820a61501ed72ba49
Markus Bauer <66630178+markus1bauer@users.noreply.github.com>
2021-03-11 16:33:05 +0100

    Update metadata

--- R/show_figure_1_map.R
+++ R/show_figure_1_map.R
@@ -19,8 +19,6 @@ library(patchwork)
 ### Start ###
 rm(list = ls())
 setwd("Z:/Documents/0_Uni/2017_Projekt_8_Schnalzaue/3_Aufnahmen_und_Ergebnisse/2018_floodplain_Schnalz/data/processed/shp_files")
-#remotes::install_github("poissonconsulting/poisspatial")
-#ps_ggmap_to_raster(background_map)
 
 
 ### Load data ###
@@ -61,21 +59,23 @@ themeMB <- function(){
 ### a Map of project site -----------------------------------------------------------------------
 (sitesGraph <- ggmap(background_terrain, 
                       base_layer = ggplot(sites2, aes(x = lon, y = lat, shape = Floodplain))) +
-    geom_point(size = 2, color = "black") +
-    coord_sf(crs = st_crs(4326)) +
-    annotate("text", x = 10.9553, y = 47.7743, label = "Inactive floodplain", size = 3) +
-    annotate("text", x = 10.9652, y = 47.77475, label = "Active floodplain", size = 3) +
-    ggspatial::annotation_scale(location = "br", pad_y = unit(0.6, "cm"), pad_x = unit(0.7, "cm"),
-                                width_hint = 0.4, height = unit(0.2, "cm")) +
-    ggspatial::annotation_north_arrow(location = "br", pad_y = unit(1.1, "cm"), pad_x = unit(0.6, "cm"), 
-                                      which_north = "true", style = ggspatial::north_arrow_fancy_orienteering(), height = unit(1, "cm"), width = unit(1, "cm")) +
-    themeMB()
+   geom_point(size = 2, color = "black") +
+   geom_point(aes(x = 10.9595, y = 47.77385), pch = 15, size = 2, color = "black") +
+   annotate("text", x = 10.9595, y = 47.7736, label = "Weir", size = 3) +
+   coord_sf(crs = st_crs(4326)) +
+   annotate("text", x = 10.9553, y = 47.7743, label = "Inactive floodplain", size = 3) +
+   annotate("text", x = 10.9652, y = 47.77475, label = "Active floodplain", size = 3) +
+   ggspatial::annotation_scale(location = "br", pad_y = unit(0.6, "cm"), pad_x = unit(0.7, "cm"),
+                               width_hint = 0.4, height = unit(0.2, "cm")) +
+   ggspatial::annotation_north_arrow(location = "br", pad_y = unit(1.1, "cm"), pad_x = unit(0.6, "cm"), 
+                                     which_north = "true", style = ggspatial::north_arrow_fancy_orienteering(), height = unit(1, "cm"), width = unit(1, "cm")) +
+   themeMB()
   )
 
 ### b Germany -----------------------------------------------------------------------
 gerGraph <- ggplot() +
    geom_sf(data = ger, fill = "transparent", colour = "black") +
-   geom_point(aes(x = 10.95948, y = 47.77405), size = 1, colour = "red") +
+   geom_point(aes(x = 10.95948, y = 47.77405), size = 1, colour = "white") +
    themeMB() +
    theme(
      plot.background = element_blank()
