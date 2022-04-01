98d28f69c61a498afdb8f0c8657b226ebea9c543
Markus Bauer <markus1.bauer@tum.de>
2022-04-01 09:25:19 +0200

    Update

--- R/_prepare_data.R
+++ R/_prepare_data.R
@@ -12,6 +12,8 @@ library(naniar) #are_na
 
 ### Start ###
 #installr::updateR(browse_news = FALSE, install_R = TRUE, copy_packages = TRUE, copy_Rprofile.site = TRUE, keep_old_packages = TRUE, update_packages = TRUE, start_new_R = FALSE, quit_R = TRUE)
+renv::snapshot()
+renv::init()
 rm(list = ls())
 setwd(here("data", "raw"))
 
